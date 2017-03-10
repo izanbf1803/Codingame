94e76d8a5c9d385cfba4d19f4c74669778b6bd28
Izan Beltrán <izanbf1803@gmail.com>
2017-03-10 13:33:02 +0100

    Update There is no Spoon - Episode 1.py

--- There is no Spoon - Episode 1.py
+++ There is no Spoon - Episode 1.py
@@ -1,13 +1,12 @@
-#By izanbf1803
 width, height = (int(input()), int(input()))
 lines = [input() for i in range(height)]
 nodes = [[j,i] for i in range(len(lines)) for j in range(len(lines[i])) if lines[i][j] == "0"]
 
 for i in nodes:
-    parentR, parentD = ("-1 -1", "-1 -1")
+    parentR, parentD = ([-1, -1], [-1, -1])
     for x in range(height,0,-1):
-        if [i[0],i[1]+x] in [j for j in nodes]: parentD = ("%s %s" % (i[0], i[1]+x))
+        if [i[0],i[1]+x] in [j for j in nodes]: parentD = [i[0], i[1]+x]
     for x in range(width,0,-1):
-        if [i[0]+x,i[1]] in [j for j in nodes]: parentR = ("%s %s" % (i[0]+x, i[1]))
+        if [i[0]+x,i[1]] in [j for j in nodes]: parentR = [i[0]+x, i[1]]
     
-    print("%s %s %s %s" % (i[0], i[1], parentR, parentD))
+    print(' '.join(map(str, i+parentR+parentD)))
