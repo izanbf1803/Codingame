c124de4579f08ed3e3883f831de034a9d7a5891b
Izan Beltrán <izanbf1803@gmail.com>
2018-09-09 00:42:47 +0200

    Remove templates not used here

--- sponsored/ANEO Sponsored Puzzle.cpp
+++ sponsored/ANEO Sponsored Puzzle.cpp
@@ -1,22 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define D(x) cerr << #x << " = " << (x) << ", "
-#define REP(i, k) for(int i = 0; i < (k); ++i)
-#define RREP(i, k) for(int i = (k)-1; i >= 0; --i)
-
-typedef unsigned int uint;
-typedef unsigned long ul;
-typedef unsigned long long ull;
-typedef long long ll;
 typedef long double ld;
-typedef unsigned char byte;
-
 template<typename T> using V = vector<T>;
-template<typename T, typename U> using umap = unordered_map<T,U>;
-template<typename T> using uset = unordered_set<T>;
-template<typename T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
-template<typename T> using max_heap = priority_queue<T>;
 
 struct light {
     int d, t;
@@ -28,7 +14,6 @@ V<light> lights;
 
 int main()
 {
-    ios::sync_with_stdio(0);
     cin >> maxkmh >> n;
     for (int i = 0; i < n; ++i) {
         light l;
